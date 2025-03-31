bcd1dce2dbda6967fc8afd19f6cbe57f449badf9
Philippa Broadbent <phillybroadbent@gmail.com>
2025-03-31 14:27:09 +0100

    Add docstrings

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,33 +1,62 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def read_json_to_dataframe(input_file):
+    """
+    Read the data from a JSON file into a Pandas dataframe.
+    Clean the data by removing any incomplete rows and sort by date.
+    
+    Args:
+        input_file (str): The path to the JSON file
+        
+    Returns:
+        eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure.
+    """
+    print(f'Reading JSON file {input_file}')
+    #Read the data from a JSON file into a pandas dataframe
+    eva_df = pd.read_json(input_file, convert_dates=['date'])
+    eva_df['eva'] = eva_df['eva'].astype(float)
+    # Clean the data by removing any incomplete rows and sort by date
+    eva_df.dropna(axis=0, inplace=True)
+    eva_df.sort_values('date', inplace=True)
+    return eva_df
+
+def write_dataframe_to_csv(df, output_file):
+    """
+    Write the dataframe to a CSV file.
+
+    Args:
+        df (pd.DataFrame): The input dataframe.
+        output_file (str): The path to the output CSV file.
+    
+    Returns:
+        None
+    """
+    print(f'Saving to CSV file {output_file}')
+    # Save dataframe to CSV file for later analysis
+    df.to_csv(output_file, index=False)
+
+# Main code
+print('--START--')
+
 # https://data.nasa.gov/resource/eva.json (with modifications)
 input_file = open('./eva-data.json', 'r',  encoding="utf-8")
 output_file = open('./eva-data.csv','w',  encoding="utf-8")
 graph_file = './cumulative_eva_graph.png'
 
+# Read the data from the JSON file
+eva_data = read_json_to_dataframe(input_file)
 
-print('--START--')
-print(f'Reading JSON file {input_file}')
-#Read the data from a JSON file into a pandas dataframe
-eva_df = pd.read_json(input_file, convert_dates=['date'])
-eva_df['eva'] = eva_df['eva'].astype(float)
-
-# Clean the data by removing any incomplete rows and sort by date
-eva_df.dropna(axis=0, inplace=True)
-eva_df.sort_values('date', inplace=True)
-
-print(f'Saving to CSV file {output_file}')
-# Save dataframe to CSV file for later analysis
-eva_df.to_csv(output_file, index=False)
+# Convert and export data to CSV file
+write_dataframe_to_csv(eva_data, output_file)
 
 print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
 #Plot cumulative time in space over the years
-eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
+eva_data['duration_hours'] = eva_data['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+eva_data['cumulative_time'] = eva_data['duration_hours'].cumsum()
 
 
-plt.plot(eva_df['date'],eva_df['cumulative_time'], 'ko-')
+plt.plot(eva_data['date'],eva_data['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
